96070d292a507bcc70f05a1b230e733aa19738cb
Anirudh Sanjeev <skyronic@gmail.com>
2013-09-19 23:06:38 +0530

    updated some hints

--- static/levels/3.js
+++ static/levels/3.js
@@ -1,7 +1,7 @@
 window.currentLevel = window.makeWarriorLevel(9, 8, [
     [0, 'warrior'],
-    [2, 'crab'],
-    [4, 'crab'],
-    [5, 'crab'],
-    [7, 'crab']
+    [2, 'troll'],
+    [4, 'troll'],
+    [5, 'troll'],
+    [7, 'troll']
 ]);
